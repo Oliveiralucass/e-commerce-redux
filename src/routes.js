84f0ce52207a784aedd48a0e7b5ae16098541315
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-21 21:06:59 -0300

    Primeiro commit, adicionado estrutura do projeto

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Carrinho } from './pages/Carrinho/Carrinho'
+import { Home } from './pages/Home/Home'
+import { NotFind } from './pages/NotFind/NotFind'
+import { Weapons } from './pages/Weapons/Weapons'
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+        <Route path='/carrinho' element={<Carrinho />} />
+
+        <Route path='/weapons'>
+          <Route index element ={<Weapons />}/>
+          <Route path='/weapons/:gun' element={<Weapons />} />
+        </Route>
+        <Route path='*' element={<NotFind />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
