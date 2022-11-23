c20cbad501fdd6a965fafde0accf26d4fb2c42af
Yuri Nunes Atzler <atzleryuri@gmail.com>
2022-11-23 01:16:42 -0300

    commit besta

--- src/pages/Weapons/Weapons.js
+++ src/pages/Weapons/Weapons.js
@@ -1,40 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import CardSkin from '../../components/CardSkin/CardSkin'
-import CardCarrinho from '../../components/CardCarrinho/CardCarrinho'
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { useParams } from "react-router-dom";
+import CardSkin from "../../components/CardSkin/CardSkin";
+import CardCarrinho from "../../components/CardCarrinho/CardCarrinho";
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./Weapons.module.css";
 
 export const Weapons = (props) => {
+  const { url, arma, nome, colecao, image } = props;
 
-  const {
-        url,
-        arma,
-        nome,
-        colecao,
-        image
-  } = props;
-
-
-  const {gun} = useParams()
-  console.log(gun)
-
-
-
+  const { gun } = useParams();
 
   const dispatch = useDispatch();
 
-  const skins = useSelector(state => state.skins.filter((skin) => {
-    return skin.url === gun
-  }))
-
-
-
-
-  console.log(skins);
+  const skins = useSelector((state) =>
+    state.skins.filter((skin) => {
+      return skin.url === gun;
+    })
+  );
 
   return (
-    <div>
-      <CardSkin nome={"teste"} imagem={"ak-47.png"} colecao={"caixa x"} preco={"R$ 10.99"}/>
+    <div className={styles.main}>
+      {skins &&
+        skins.map((skin) => {
+          return (
+            <CardSkin
+              nome={skin.nome}
+              imagem={skin.image}
+              colecao={skin.colecao}
+              preco={"R$ 999"}
+            />
+          );
+        })}
+      {/* <CardSkin
+        nome={"teste"}
+        imagem={"ak-47.png"}
+        colecao={"caixa x"}
+        preco={"R$ 10.99"}
+      /> */}
     </div>
-  )
-}
+  );
+};
