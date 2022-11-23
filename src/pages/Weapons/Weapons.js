914c25c044777539d5e461315133fe6a8eab4a67
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-23 01:08:35 -0300

    Adicionado skins na page Weapons

--- src/pages/Weapons/Weapons.js
+++ src/pages/Weapons/Weapons.js
@@ -2,10 +2,35 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import CardSkin from '../../components/CardSkin/CardSkin'
 import CardCarrinho from '../../components/CardCarrinho/CardCarrinho'
+import { useDispatch, useSelector } from 'react-redux'
 
-export const Weapons = () => {
+export const Weapons = (props) => {
+
+  const {
+        url,
+        arma,
+        nome,
+        colecao,
+        image
+  } = props;
+
+
   const {gun} = useParams()
   console.log(gun)
+
+
+
+
+  const dispatch = useDispatch();
+
+  const skins = useSelector(state => state.skins.filter((skin) => {
+    return skin.url === gun
+  }))
+
+
+
+
+  console.log(skins);
 
   return (
     <div>
