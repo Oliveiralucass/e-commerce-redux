74ac31bc637ea2a91865b75c89e17b275571f40d
Yuri Nunes Atzler <atzleryuri@gmail.com>
2022-11-24 13:08:37 -0300

    finalizar compra

--- src/pages/FinalizarPedido/FinalizarPedido.js
+++ src/pages/FinalizarPedido/FinalizarPedido.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./FinalizarPedido.module.css";
+import { resetarCarrinho } from "../../store/reducers/carrinho";
 
 export const FinalizarPedido = () => {
   const { state } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
+      <h1>
+        Número do Pedido: <span>#471232</span>
+      </h1>
       {state.items &&
         state.items.map((skin) => {
           return (
@@ -15,10 +22,24 @@ export const FinalizarPedido = () => {
                 <img src={require(`../../assets/${skin.image}`)} alt="imagem" />
                 <p>{skin.nome}</p>
               </div>
-              <p>{skin.quantidade}</p>
+              <div className={styles.quantidadePreco}>
+                <p>R$ {skin.preco}/uni</p>
+                <p>{skin.quantidade}</p>
+              </div>
             </div>
           );
         })}
+      <h3>
+        Preço total: <span>R$ {state.precoTotal}</span>{" "}
+      </h3>
+      <button
+        onClick={() => {
+          dispatch(resetarCarrinho());
+          navigate("/");
+        }}
+      >
+        Concluir
+      </button>
     </div>
   );
 };
--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -23,47 +23,52 @@ const carrinhoSlice = createSlice({
     },
 
     removerDoCarrinho: (state, { payload }) => {
-      let skinPrecoAcumulado = payload.preco * payload.quantidade
+      let skinPrecoAcumulado = payload.preco * payload.quantidade;
       state.precoTotal -= skinPrecoAcumulado;
       state.items = state.items.filter((skin) => skin.id !== payload.id);
     },
 
     adicionarQuantidade: (state, { payload }) => {
       console.log(payload);
-        state.items = state.items.map((skinCarrinho) => {
-            if(skinCarrinho.id === payload.id) {
-                skinCarrinho.quantidade += 1;
-                state.precoTotal += payload.preco
-            }   
-            return skinCarrinho
-        })
+      state.items = state.items.map((skinCarrinho) => {
+        if (skinCarrinho.id === payload.id) {
+          skinCarrinho.quantidade += 1;
+          state.precoTotal += payload.preco;
+        }
+        return skinCarrinho;
+      });
     },
 
     removerQuantidade: (state, { payload }) => {
-        state.items = state.items.map((skinCarrinho) => {
-          if(skinCarrinho.quantidade > 1) {
-            if(skinCarrinho.id === payload.id) {
-                skinCarrinho.quantidade -= 1;
-                state.precoTotal -= payload.preco
-            }   
+      state.items = state.items.map((skinCarrinho) => {
+        if (skinCarrinho.quantidade > 1) {
+          if (skinCarrinho.id === payload.id) {
+            skinCarrinho.quantidade -= 1;
+            state.precoTotal -= payload.preco;
           }
-            return skinCarrinho
-        })
+        }
+        return skinCarrinho;
+      });
     },
 
     mudarFavoritoCarrinho: (state, { payload }) => {
       state.items = state.items.map((skin) => {
-        if(skin.id === payload)
-          skin.favorito = !skin.favorito;
-          return skin
-      })
-
+        if (skin.id === payload) skin.favorito = !skin.favorito;
+        return skin;
+      });
     },
 
     resetarCarrinho: () => initialState,
   },
 });
 
-export const { adicionarAoCarrinho, removerDoCarrinho, adicionarQuantidade ,removerQuantidade, mudarFavoritoCarrinho } = carrinhoSlice.actions;
+export const {
+  adicionarAoCarrinho,
+  removerDoCarrinho,
+  adicionarQuantidade,
+  removerQuantidade,
+  mudarFavoritoCarrinho,
+  resetarCarrinho,
+} = carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
