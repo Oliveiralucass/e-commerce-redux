4af98d31f35faaa99cdcb5e6451cd31dbc93def4
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-24 14:54:47 -0300

    Adicionado 404

--- src/pages/NotFind/NotFind.js
+++ src/pages/NotFind/NotFind.js
@@ -1,5 +1,11 @@
 import React from "react";
+import styles from './NotFind.module.css'
 
 export const NotFind = () => {
-  return <div>NotFind</div>;
+  return <div className={styles.container}>
+    
+    <h1>404</h1>  
+    <p>Página não encontrada</p>
+
+  </div>;
 };
